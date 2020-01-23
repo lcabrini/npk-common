50d1842a13f8f59d682f6d7c9e284530e1e3c88b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-01-23 18:59:46 +0000

    Display login form.

--- routes.go
+++ routes.go
@@ -1,16 +1,9 @@
 package npk
 
 import (
-    "fmt"
-    "net/http"
     "github.com/gorilla/mux"
 )
 
 func SetupRoutes(r *mux.Router) {
     r.HandleFunc("/login", login)
-}
-
-// Temporary
-func login(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Login")
 }
--- user.go
+++ user.go
@@ -1,11 +1,36 @@
 package npk
 
 import (
+    "fmt"
     "github.com/google/uuid"
+    "net/http"
+    "text/template"
 )
 
 type User struct {
-    Id uuid.UUID
+    Id       uuid.UUID
     Username string
     Password string
+}
+
+var loginForm = `
+<form>
+<label>Username</label>
+<input type="text" name="username"><br>
+<input type="password" name="password"><br>
+</form>
+`
+
+func login(w http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+    case "GET":
+        t, _ := template.New("loginForm").Parse(loginForm)
+        t.ExecuteTemplate(w, "loginForm", nil)
+
+    case "POST":
+        fmt.Fprintf(w, "login post")
+
+    default:
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    }
 }

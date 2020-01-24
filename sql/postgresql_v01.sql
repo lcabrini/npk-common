a505a7b273f3bbeeea08bdf4f8c5b0aa67170d7d
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-01-24 21:55:46 +0000

    Added status field to users table.

--- sql/postgresql_v01.sql
+++ sql/postgresql_v01.sql
@@ -1,15 +1,24 @@
+drop type if exists user_status;
+create type user_status as enum(
+    'new',
+    'active',
+    'inactive',
+    'deleted'
+);
+
 drop table if exists users;
 create table users(
     id uuid,
     username varchar(50) not null,
     password varchar(50) not null,
+    status user_status not null,
     primary key(id),
     unique(password)
 );
 
-insert into users(id, username, password) values(
+insert into users(id, username, password, status) values(
     '6c6cc02e-cca6-451d-9928-1f4d898c838e',
     'sa',
-    '%npK-s3r3T%'
+    '%npK-s3Kr3T%',
+    'new'
 );
-

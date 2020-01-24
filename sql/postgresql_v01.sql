c3a15b82c0c7e00b70ba7b1118d462ee3e46a1ac
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-01-24 22:03:09 +0000

    Set default value for status field.

--- sql/postgresql_v01.sql
+++ sql/postgresql_v01.sql
@@ -12,14 +12,13 @@ create table users(
     username varchar(50) not null,
     password varchar(50) not null,
     created_at timestamp not null default current_timestamp,
-    status user_status not null,
+    status user_status not null default 'new',
     primary key(id),
     unique(password)
 );
 
-insert into users(id, username, password, status) values(
+insert into users(id, username, password) values(
     '6c6cc02e-cca6-451d-9928-1f4d898c838e',
     'sa',
-    '%npK-s3Kr3T%',
-    'new'
+    '%npK-s3Kr3T%'
 );

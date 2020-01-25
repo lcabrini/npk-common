0cdc3c4d5931f809f71663b0db381637df210bba
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-01-25 19:57:20 +0000

    Implemented DBConnection function.

---
+++ database.go
@@ -0,0 +1,31 @@
+package npk
+
+import (
+    "fmt"
+    "database/sql"
+    _ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+func DBConnection(config Configuration) *sql.DB {
+    if db == nil {
+        var err error
+
+        info := fmt.Sprintf(`host=%s port=%s user=%s password=%s
+        dbname=%s sslmode=%s`,
+        config.Database.Host,
+        config.Database.Port,
+        config.Database.Username,
+        config.Database.Password,
+        config.Database.Catalog,
+        config.Database.SSLMode)
+
+        db, err = sql.Open("postgres", info)
+        if err != nil {
+            panic(err)
+        }
+    }
+
+    return db
+}

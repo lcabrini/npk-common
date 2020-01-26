5a2c1ad9d7df07b794e84cca51b96909eaece656
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-01-26 22:43:30 +0000

    Fix: set session options.

--- session.go
+++ session.go
@@ -8,3 +8,12 @@ var (
     key = []byte("12345678123456781234567812345678")
     Store = sessions.NewCookieStore(key)
 )
+
+func init() {
+    Store.Options = &sessions.Options{
+        Domain: "localhost",
+        Path: "/",
+        MaxAge: 3600 * 3,
+        HttpOnly: true,
+    }
+}

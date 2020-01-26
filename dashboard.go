2daec0862e7f5ea447acb47a1eb9b4999bcc96e5
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-01-26 21:34:41 +0000

    Fix: wrong operator.

--- dashboard.go
+++ dashboard.go
@@ -3,13 +3,13 @@ package npk
 import (
     "fmt"
     "net/http"
-    "github.com/google/uuid"
+    // "github.com/google/uuid"
 )
 
 func dashboard(w http.ResponseWriter, r *http.Request) {
     session, _ := Store.Get(r, "npk-cookie")
     uid := session.Values["user"]
-    if uid := nil {
+    if uid == nil {
         fmt.Fprint(w, "no user")
     } else {
         fmt.Fprintf(w, "user: %s", uid)

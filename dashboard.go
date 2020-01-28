330684b9facf95420d2ef8a3f8c5ebe29ef93237
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-01-28 20:45:49 +0000

    Check for session errors in dashboard().

--- dashboard.go
+++ dashboard.go
@@ -2,12 +2,18 @@ package npk
 
 import (
     "fmt"
+    "log"
     "net/http"
     // "github.com/google/uuid"
 )
 
 func dashboard(w http.ResponseWriter, r *http.Request) {
-    session, _ := Store.Get(r, "npk-cookie")
+    session, err := Store.Get(r, "npk-cookie")
+    if err != nil {
+        log.Printf("Unable to read session: %v", err)
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+
     uid := session.Values["user"]
     if uid == nil {
         fmt.Fprint(w, "no user")
